380748560c65626c29afe24f82b52d39a1246cae
GregoryValsaint <gvalsaint@live.fr>
2020-12-11 11:23:43 +0100

    greg

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Inscription</h1>
+    <p>
+      <label>Email</label>
+      <input ref="myInput" type="text" v-model="currentEmail">
+    </p>
+    <p>
+      <label>Password</label>
+      <input ref="myInput" type="text" v-model="currentPassword">
+    </p>
+    <p>
+      <input type="submit"  @click="addUser">
+    </p>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  name: "RegisterForm",
+  data() {
+    return {
+      currentUser: [],
+      currentEmail: "",
+      currentPassword: ""
+    }
+  },
+  methods: {
+    async addUser() {
+      const response = await Axios.post("https://127.0.0.1:8000/api/register",{
+        email: this.currentEmail,
+        password: this.currentPassword
+      })
+      this.currentUser.push(response.data)
+      this.$refs.myInput.focus()
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Inscription.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <RegisterForm/>
+</div>
+</template>
+
+<script>
+import RegisterForm from "@/components/RegisterForm";
+export default {
+name: "Inscription",
+  components: {RegisterForm}
+}
+</script>
+
+<style scoped>
+
+</style>
