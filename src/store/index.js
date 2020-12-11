2d8802818b0ebd4619b2e0c8a818d922bbdad372
Delphine1988 <donnedelphine@hotmail.fr>
2020-12-11 09:41:59 +0100

    store token

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        token: localStorage.getItem('user-token') || '',
+        status: '',
+    },
+    getters: {
+        isConnected(state){
+            if(state.token === null){
+                return false
+            }else{
+                return true;
+            }
+        },
+
+        // isAuthenticated: state => !!state.token,
+        // authStatus: state => state.status
+    },
+    mutations: {
+        logToken: (state)=> {
+            state.status = 'loading'
+        },
+        logTokenSuccess: (state, token) => {
+            state.status = 'success'
+            state.token = token
+        },
+        logTokenError: (state) => {
+            state.status = 'error'
+        }
+    },
+    actions: {
+        async logToken({commit}, user) {
+          commit('logToken');
+            try {
+                const response = await Axios.post('http://127.0.0.1:8000/api/login_check', {
+                    "username": "william@codecolliders.com",
+                    "password": "password"
+                })
+                this.token = response.data.token;
+                localStorage.setItem('user-token', this.token)
+               commit('logTokenSuccess', this.token)
+            } catch {
+               commit('logTokenError', error)
+                localStorage.removeItem('user-token')
+            }
+        }
+    },
+    modules: {}
 })
---
+++ src/views/ListProduct.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ListProduct"
+    }
+</script>
+
+<style scoped>
+
+</style>
