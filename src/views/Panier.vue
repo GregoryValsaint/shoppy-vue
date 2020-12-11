c5c20fffee1197c271cd6a80d3ad7f861cf3e90a
SandraLeRoi <oawx72@gmail.com>
2020-12-11 11:28:47 +0100

    panier

--- src/views/Panier.vue
+++ src/views/Panier.vue
@@ -1,18 +1,24 @@
 <template>
-<div>
   <div>
-    Votre commande est la suivante :
-
+    <div class="cart">
+      Votre commande est la suivante :
+      <span v-for="order in cart.orderLines"> Nom: {{order.product.name}}
+        Prix à l'unité: {{order.product.price}}
+        Quantité : {{order.quantity}} </span>
+      <p> Total :{{cart.total}}</p>
+    </div>
+    <div class="user">
+      <p> Email :{{cart.user.email}}</p>
+    </div>
     <div>{{cart}}</div>
   </div>
-</div>
 </template>
 
 <script>
 import Axios from "axios";
 
 export default {
-name: "Panier",
+  name: "Panier",
   data() {
     return {
       cart: null,
@@ -20,18 +26,13 @@ name: "Panier",
   },
   methods: {
     async fetchCart() {
-      const response = await Axios.get('http://127.0.0.0:8000/api/get-cart', {
-        headers: {
-          'Bearer ': this.$store.token
-        }
-      });
-      this.cart = response.data;
+      const response = await Axios.get("http://127.0.0.1:8000/api/get-cart")
+      this.cart = response.data
       console.log(response.data);
     }
   },
-  mounted() {
-    this.fetchCart();
-
+  async mounted() {
+    await this.fetchCart();
   },
 };
 </script>
