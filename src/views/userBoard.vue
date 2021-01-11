9b77262893907f7b362e105205b068c12a893f87
SandraLeRoi <oawx72@gmail.com>
2021-01-11 11:45:16 +0100

    modification userBoard

--- src/views/userBoard.vue
+++ src/views/userBoard.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-    {{ this.user.email}}
+      <p> Email :{{ this.user.email}}</p>
+      <p> Nom : {{ this.user.name}}</p>
+      <p> {{this.listCart.status}}</p>
+      <p> Total commande :{{this.listCart.total}}</p>
+      <li v-for="line in this.listCart.orderLines"> Quantité :{{line.quantity}}, Nom: {{line.product.name}}, Prix unitaire :{{line.product.price}}, Total :{{ line.total}}</li>
+
     </div>
 </template>
 
@@ -12,7 +17,7 @@
         data() {
             return {
                 user: "",
-                listCart: []
+                listCart: ""
             }
         },
         methods: {
@@ -20,10 +25,16 @@
                 const response = await Axios.get(process.env.VUE_APP_API_URL+'/api/account')
                 this.user = response.data
                 console.log(response)
-            }
+            },
+          async getCart() {
+              const response = await Axios.get(process.env.VUE_APP_API_URL+'/api/get-cart')
+              this.listCart = response.data
+              console.log (response)
+          }
         },
         mounted() {
             this.getUser()
+            this.getCart()
         }
     }
 </script>
